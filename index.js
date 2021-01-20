57214ecbcf0d2fdf5ebbdfc7cde90aafa8d06096
Max Liebe <amalox@outlook.com>
2021-01-20 20:59:09 +0100

    Connected the server to the Arduino via serial

--- index.js
+++ index.js
@@ -1,18 +1,31 @@
 const express = require('express');
 const fs = require('fs');
+const SerialPort = require('serialport');
+const Readline = SerialPort.parsers.Readline;
 const app = express();
 const port = 3000;
 const database = JSON.parse(fs.readFileSync('default-database.json'));
 
-let temperature = 0;
-let humidity = 0;
+let temperature = '0';
+let humidity = '0';
+
+SerialPort.list()
+    .then(ports => {
+        let comPort = ports.filter(port => port.manufacturer.includes('arduino'))[0];
+        let serial = new SerialPort(comPort.path, { baudRate: 9600 });
+        let parser = serial.pipe(new Readline());
+        parser.on('data', data => {
+            let parsed = data.split(/T|H|\r/);
+            parsed.shift();
+            temperature = parseFloat(parsed[0]).toFixed(1);
+            humidity = parseFloat(parsed[1]).toFixed(1);
+            console.log(`Got new values. Temperature: ${temperature}, humidity: ${humidity}`);
+        });
+    });
 
 app.use(express.json());
 app.get('/home', (req, res) => {
-    res.send({
-        temperature: 19,
-        humidity: 40
-    });
+    res.send({ temperature, humidity });
 });
 
 app.get('/pantry', (req, res) => {
