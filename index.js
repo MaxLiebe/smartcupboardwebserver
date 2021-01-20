41a12dc884cd9adeb199bd33a05e818f9fc64326
Max Liebe <amalox@outlook.com>
2021-01-20 18:26:11 +0100

    Yoink

--- index.js
+++ index.js
@@ -1,15 +1,32 @@
 const express = require('express');
+const fs = require('fs');
 const app = express();
 const port = 3000;
+const database = JSON.parse(fs.readFileSync('default-database.json'));
+
+let temperature = 0;
+let humidity = 0;
 
 app.use(express.json());
-app.get('/main', (req, res) => {
+app.get('/home', (req, res) => {
     res.send({
         temperature: 19,
         humidity: 40
     });
 });
 
+app.get('/pantry', (req, res) => {
+    res.send(database.pantry);
+});
+
+app.get('/list', (req, res) => {
+    res.send(database.shoppinglist);
+});
+
+app.get('/recipies', (req, res) => {
+    res.send(database.recipies);
+});
+
 app.listen(port, () => {
-    console.log(`Example app listening at http://localhost:${port}`);
+    console.log(`Server listening at port ${port}`);
 });
